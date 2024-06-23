7bcf0c0575a67acbd14ee84235af0d1b9908f6d3
Priyanshu <sethipriyanshu@outlook.com>
2024-06-23 02:33:08 -0500

    add helloHandler function

--- web-server/main.go
+++ web-server/main.go
@@ -6,6 +6,23 @@ import (
 	"net/http"
 )
 
+// every api/route has a response or a request and that is what the parameters we pass signify * is a pointer to the request user sends
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// handling the situation if the request does not come from hello route
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	// we dont want people to post to /hello but only be able to get
+	if r.Method != "GET" {
+		http.Error(w, "Method is not Supported", http.StatusNotFound)
+		return
+	}
+	// Fprintf function formats and writes to w
+	fmt.Fprintf(w, "Hello!")
+
+}
+
 func main() {
 	// initialize a file server using the http import and set it to static directory
 	fileServer := http.FileServer(http.Dir("./static"))
