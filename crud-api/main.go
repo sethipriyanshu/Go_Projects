a1fb90b5ee0e6e0888bdf16a61ae864a83329d85
Priyanshu <sethipriyanshu@outlook.com>
2024-06-27 06:02:08 -0500

    add update function

--- crud-api/main.go
+++ crud-api/main.go
@@ -76,6 +76,22 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+// UpdateMovie function
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	// declare a new Route
 	r := mux.NewRouter()
