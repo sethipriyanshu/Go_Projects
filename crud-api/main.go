45df6ddaa6116b2cc7516be16dd45bc55b2d21df
Priyanshu <sethipriyanshu@outlook.com>
2024-06-27 05:52:51 -0500

    add crd functions

--- crud-api/main.go
+++ crud-api/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"encoding/json" // help encode data
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
-	//"encoding/json"  // help encode data
-	//"math/rand"
-	//"net/http"
-	//"strconv"
-	//"github.com/gorilla/mux"
 )
 
 // declare a struct of type movie
@@ -30,10 +28,61 @@ type Director struct {
 // declare a slice to store the movies (not using a backend database for now)
 var movies []Movie
 
+// GetMovies Function
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+// DeleteMovie function
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// using append to delete an item in a slice
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+// GetMovie Function
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(movies[index])
+			break
+		}
+	}
+}
+
+// CreateMovie function
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content=Type", "application/json")
+	var movie Movie
+	// we will be passing movie data in body when making the api request
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
 func main() {
 	// declare a new Route
 	r := mux.NewRouter()
 
+	// add some movies to make sure the code functions
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438229", Title: "Movie Two", Director: &Director{Firstname: "Lorem", Lastname: "Ipsum"}})
 	// handling functions on respective routes
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
