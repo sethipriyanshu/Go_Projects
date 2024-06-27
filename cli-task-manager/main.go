1ac4b145b60b7abf303365a4d4f73b55880424a1
Priyanshu <sethipriyanshu@outlook.com>
2024-06-27 10:11:53 -0500

    add ui logic

--- cli-task-manager/main.go
+++ cli-task-manager/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Print("**************************************\n Weclome to your daily task manager! \n")
+	fmt.Print("**************************************\n Weclome to your daily task manager! \n**************************************\n")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Choose one of the following options:-\n1:- Create\n2:- View\n3:- Update\n4:- Delete\n5:- Quit\n")
+		userChoice, err := reader.ReadString('\n')
+		userChoice = strings.TrimSpace(strings.ToLower(userChoice))
+		if err != nil {
+			fmt.Println("An Unexpected Error has occured")
+			break
+		}
+		if userChoice == "quit" {
+			fmt.Println("Exiting the Program!")
+			break
+		} else if userChoice == "create" {
+
+		} else if userChoice == "view" {
+
+		} else if userChoice == "update" {
+
+		} else if userChoice == "delete" {
+
+		} else {
+
+		}
+	}
 }
